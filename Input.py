284ed992cc3575ea98f398d7d9280a974606b34d
Your Name <your.email@example.com>
2025-09-14 19:07:50 +0530

    Add Day 2: learned input(), len(), and variables

---
+++ Input.py
@@ -0,0 +1,25 @@
+#Day 2 practice: input, len, and variables
+
+
+#input Fuction
+print("Hello" + input("What is your name?"))
+
+
+#length Function
+print(len(input("what's your name?")))
+
+
+#python variables
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+
+#switch values in variables
+a = input("Enter the value for a: ")
+b = input("Enter the value for b: ")
+c = a 
+a = b 
+b = c
+print("a: " + a)
+print("b: " + b)
